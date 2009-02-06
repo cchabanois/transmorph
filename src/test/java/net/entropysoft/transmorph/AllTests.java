0b9d094fd94c59c1088ce23d9502d759c5003dc9
Cedric Chabanois <cchabanois@gmail.com>
2009-02-06 19:25:18 +0000

    added UsedConvertersTest to the test suite

--- src/test/java/net/entropysoft/transmorph/AllTests.java
+++ src/test/java/net/entropysoft/transmorph/AllTests.java
@@ -2,15 +2,13 @@ package net.entropysoft.transmorph;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import net.entropysoft.transmorph.signature.ClassFactoryTest;
-import net.entropysoft.transmorph.signature.JavaTypeToTypeSignatureTest;
-import net.entropysoft.transmorph.signature.TypeSignatureFactoryTest;
-import net.entropysoft.transmorph.signature.TypeSignatureParserTest;
+import net.entropysoft.transmorph.context.UsedConvertersTest;
 
 public class AllTests {
 
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for net.entropysoft.transformers");
+        suite.addTestSuite(UsedConvertersTest.class);
         suite.addTest(net.entropysoft.transmorph.signature.AllTests.suite());
         suite.addTest(net.entropysoft.transmorph.type.AllTests.suite());
         suite.addTest(net.entropysoft.transmorph.converters.AllTests.suite());

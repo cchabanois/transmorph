110976a9d408d674a1ec9f2f50da35bd25b09208
Cedric Chabanois <cchabanois@gmail.com>
2009-04-30 19:36:38 +0000

    added test

--- src/test/java/net/entropysoft/transmorph/injectors/MapToBeanTest.java
+++ src/test/java/net/entropysoft/transmorph/injectors/MapToBeanTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.entropysoft.transmorph.ConverterException;
 import net.entropysoft.transmorph.DefaultConverters;
 import net.entropysoft.transmorph.IBeanInjector;
 import net.entropysoft.transmorph.TransmorphBeanInjector;
@@ -12,13 +13,14 @@ import junit.framework.TestCase;
 
 public class MapToBeanTest extends TestCase {
 
-	public void testBeanInjectionFromMap() throws Exception {
+	public void testMapToBean() throws Exception {
 		MyBean1 myBean1 = new MyBean1();
 		IBeanInjector beanInjector = new MapToBeanInjector();
 		beanInjector.setPropertyValueConverter(new DefaultConverters());
-		
-		TransmorphBeanInjector transmorphBeanInjector = new TransmorphBeanInjector(beanInjector);
-		Map<String,Object> properties = new HashMap<String, Object>();
+
+		TransmorphBeanInjector transmorphBeanInjector = new TransmorphBeanInjector(
+				beanInjector);
+		Map<String, Object> properties = new HashMap<String, Object>();
 		properties.put("myInt", "55");
 		properties.put("myListOfStrings", new int[] { 1, 2, 3 });
 
@@ -29,6 +31,26 @@ public class MapToBeanTest extends TestCase {
 		assertEquals("2", listOfStrings.get(1));
 		assertEquals("3", listOfStrings.get(2));
 		assertEquals(null, myBean1.getMyBean2());
-	}	
-	
+	}
+
+	public void testMapToBeanTooManyProperties() throws Exception {
+		MyBean1 myBean1 = new MyBean1();
+		IBeanInjector beanInjector = new MapToBeanInjector();
+		beanInjector.setPropertyValueConverter(new DefaultConverters());
+
+		TransmorphBeanInjector transmorphBeanInjector = new TransmorphBeanInjector(
+				beanInjector);
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("myInt", "55");
+		properties.put("myListOfStrings", new int[] { 1, 2, 3 });
+		properties.put("myInt2", "55");
+
+		try {
+			transmorphBeanInjector.inject(properties, myBean1);
+			fail("Should not have been able to inject");
+		} catch (ConverterException e) {
+
+		}
+	}
+
 }
